00d117a44283b8a9c9ebeebcd16ca6288954f935
sandasagar <sandasagar4@gmail.com>
2019-07-31 22:45:42 -0400

    Added layout for login and its implementation in activity.

--- app/src/main/java/com/example/pragnas/login/LoginActivity.java
+++ app/src/main/java/com/example/pragnas/login/LoginActivity.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.pragnas.MainActivity;
 import com.example.pragnas.R;
 import com.example.pragnas.activitylifecycle.ActivityA;
 import com.example.pragnas.explicitintent.IntentActivity;
@@ -14,7 +16,9 @@ import com.example.pragnas.implicitIntent.ImplicitIntent;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Button btnExplicitIntent;
+    EditText username;
+    EditText password;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +29,18 @@ public class LoginActivity extends AppCompatActivity {
 
     void initViews() {
 
+        username = (EditText)findViewById(R.id.etUsername);
+        password = (EditText)findViewById(R.id.etPassword);
+        login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               if (username.getText().toString().isEmpty() && password.getText().toString().isEmpty()) {
+                   Intent to_relative = new Intent(LoginActivity.this, MainActivity.class);
+                   startActivity(to_relative);
+               }
+           }
+
+       });
     }
 }
