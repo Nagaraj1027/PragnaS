a11d2e41022eddb9168c6d19ad9ba41ca0d1af15
Nagaraj1027 <nagaraj.pambhala@gmail.com>
2019-08-03 12:24:25 +0530

    Merge branch 'master' of https://github.com/Nagaraj1027/PragnaS
    # Conflicts:
    #	app/src/main/res/layout/login_activity.xml

--- app/src/main/java/com/example/pragnas/appclass/ApplicationClass.java
+++ app/src/main/java/com/example/pragnas/appclass/ApplicationClass.java
@@ -17,13 +17,33 @@ public class ApplicationClass extends Application {
         editor = sharedPreferences.edit();
     }
 
-  public   static void putUserNameIntoPreferences(String name) {
+    public static void putUserNameIntoPreferences(String name) {
         editor.putString("USERNAME", name);
         editor.commit();
     }
 
-   public static String getUserNameFromSharedPreferences() {
+    public static String getUserNameFromSharedPreferences() {
         String name = sharedPreferences.getString("USERNAME", null);
         return name;
+    }
+
+    public static void putPasswordIntoPreferences(String pasword) {
+        editor.putString("PASSWORD", pasword);
+        editor.commit();
+    }
+
+    public static String getPasswordFromSharedPreferences() {
+        String name = sharedPreferences.getString("PASSWORD", null);
+        return name;
+    }
+
+    public static void putLoginStatusIntoPreferences(Boolean lStatus) {
+        editor.putBoolean("LOGINSTATUS", lStatus);
+        editor.commit();
+    }
+
+    public static Boolean getLoginStatusFromSharedPreferences() {
+        Boolean lStatus = sharedPreferences.getBoolean("LOGINSTATUS", false);
+        return lStatus;
     }
 }
